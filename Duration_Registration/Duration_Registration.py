1ae96c63288fa48da2aa00fc3f369605297db3e9
Jeremy Gunawan <jemygunawanliu@gmail.com>
2025-08-07 21:32:42 +0700

    new file:   Duration_Registration/Duration_Registration.py

---
+++ Duration_Registration/Duration_Registration.py
@@ -0,0 +1,14 @@
+import os
+import time
+import datetime
+
+"""
+Study Case:
+We have a gym, and we want to track how long each person spends their time in the gym.
+
+1. We make a system that tracks the time each person enters and exits the gym.
+2. By typing their name into the system, with the datetime, and of course, the time.
+3. We track the time when the name is type into the system.
+4. Also i will make database for user that already become a member, so that we can track our member!
+
+"""
