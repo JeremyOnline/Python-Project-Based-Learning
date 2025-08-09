0163fc784671907b9ddb6d32b55c1729cd5a8d11
Jeremy Gunawan <jemygunawanliu@gmail.com>
2025-08-09 22:14:23 +0700

    modified:   Duration_Registration/Duration_Registration.py

--- Duration_Registration/Duration_Registration.py
+++ Duration_Registration/Duration_Registration.py
@@ -1,4 +1,5 @@
 import os
+import sqlite3
 import time
 import datetime
 
@@ -13,10 +14,38 @@ We have a gym, and we want to track how long each person spends their time in th
 
 """
 
-def user_In(name, time):
-    print(f"Welcome {name} to the gym! You entered at {time}")
+BASE_DIR = os.path.dirname(__file__)
+USERS_DATABASE = os.path.join(BASE_DIR, "Duration_Registration.txt")
 
-def user_out(name, time):
+# Print Out the Database
+def database_out():
+    users = []
+    if os.path.exists(USERS_DATABASE):
+        with open(USERS_DATABASE, "r") as file:
+            for line in file:
+                username, password = line.strip().split(",")
+                users.append((username, password))
+    return users
+
+# Save New User to Database
+def saveUser(name, time_user):
+    with open(USERS_DATABASE, "a") as file:
+        file.write(f"{name},{time_user}\n")
+
+# transaksi = {
+#             'deskripsi': deskripsi,
+#             'section': section,
+#             'jenis': jenis,
+#             'jumlah': jumlah,
+#             'tanggal': tanggal
+#         }
+        
+
+def user_In(name, time_user):
+    print(f"Welcome {name}!, You entered at {time_user}")
+    saveUser(name, time_user)
+
+def user_out(name, time_user):
     print(f"See you again, {name}")
 
 def main():
@@ -28,12 +57,18 @@ def main():
         choice = input("What would you like to do? ")
 
         if choice == "1":
-            name = input("What is your name? ")
-            time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            user_In(name, time)
-        if choice == "2":
-            name = input("What is your name? ")
-            time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            user_out(name, time)
+            name = input("What is your name? : ").capitalize()
+            time_user = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            user_In(name, time_user)
+        elif choice == "2":
+            name = input("What is your name? ").capitalize()
+            time_user = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            user_out(name, time_user)
+        elif choice == "4":
+            print("Goodbye!, See you again , Admin")
+            for i in range(5, 0, -1):
+                print(f"Exiting in {i} seconds...")
+                time.sleep(1)
+            break
 
 main()
