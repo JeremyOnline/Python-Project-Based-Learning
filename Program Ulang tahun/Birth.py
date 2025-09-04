011f88beb4bedbeaa0e5e0e9e7acd491d0086370
Jeremy Gunawan <jemygunawanliu@gmail.com>
2025-09-04 16:18:35 +0700

    new file:   Program Ulang tahun/Birth.py

---
+++ Program Ulang tahun/Birth.py
@@ -0,0 +1,13 @@
+"""
+Program ini dibuat untuk:
+1. Mengingatkan dan memberitahu User mengenai hari ulang tahun dan umurnya..
+2. Menyimpan list Tgl ulang tahun temannya
+Output:
+Berupa fungsi yang akan menanyakan kepada user mengenai:
+1. Input Ultah temannya
+2. Melihat list Ultah
+3. Mengecek apakah ada yg bakalan ultah dalam skala periode baik minggu, bulan, atau hari.
+
+EXTRA:
+1. Memberi pesan kepada user apabila temannya ultah hari ini!
+"""
