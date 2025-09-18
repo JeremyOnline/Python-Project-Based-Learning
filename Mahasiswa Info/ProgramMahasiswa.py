d57a35f4c620bf0a0fcab7ede529f899eae96800
Jeremy Gunawan <jemygunawanliu@gmail.com>
2025-09-18 15:12:58 +0700

    modified:   Mahasiswa Info/ProgramMahasiswa.py

--- Mahasiswa Info/ProgramMahasiswa.py
+++ Mahasiswa Info/ProgramMahasiswa.py
@@ -1,4 +1,4 @@
-
+import os
 """
 Program yang dapat menerima masukan nama,
 tempat-tanggal lahir, 
@@ -6,3 +6,10 @@ nim mahasiswa,
 dan rata-rata nilai
 """
 
+def halo():
+    nama_mahasiswa = (input("Nama Mahasiswa: "))
+    
+    return nama_mahasiswa
+
+halo()
+    
