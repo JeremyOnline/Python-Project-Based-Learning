996c58eccadf74f0dcdc0f75b76aae1ffec1387a
Jeremy Gunawan <jemygunawanliu@gmail.com>
2025-11-25 15:52:03 +0700

    modified:   Mahasiswa Info/ProgramMahasiswa.py

--- Mahasiswa Info/ProgramMahasiswa.py
+++ Mahasiswa Info/ProgramMahasiswa.py
@@ -7,9 +7,15 @@ dan rata-rata nilai
 """
 
 def halo():
-    nama_mahasiswa = (input("Nama Mahasiswa: "))
-    
-    return nama_mahasiswa
-
+    nama_mahasiswa = input("Nama Mahasiswa: ")
+    nim = input("Masukkan NIM(xxxxxx): ")
+    while True:
+        if nim == "tes":
+            print("OK")
+            break
+        else:
+            print("incorrect")
+            break
+            
 halo()
     
